6f73923c63d8cd436386c14b946f8680ed7f4033
neels2708 <neelakandanbt@gmail.com>
2022-10-18 13:49:05 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -21,7 +21,7 @@ def run_query(query):
         cur.execute(query)
         return cur.fetchall()
 
-# rows = run_query("select * from FEE_PAYMENTS_VW;")
+rows = run_query("select * from FEE_PAYMENTS_VW;")
 
 # Print results.
 # for row in rows:
