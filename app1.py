4625de749fef5b279286924e0c6c68d6ca7b918c
neels2708 <neelakandanbt@gmail.com>
2022-10-20 13:02:56 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -24,5 +24,5 @@ def run_query(query):
 rows = run_query("SELECT * from FEE_PAYMENTS_VW;")
 
 # Print results.
-# for row in rows:
-#     st.write(f"{row[0]} has a :{row[1]}:")
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
