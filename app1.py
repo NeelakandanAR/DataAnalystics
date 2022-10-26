80b30e8d8fb8d15d4d6f6ff1c8d43b237ee4c655
neels2708 <neelakandanbt@gmail.com>
2022-10-26 23:00:03 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -222,6 +222,7 @@ if result == 'Weekend/Weekday preferences':
     # b1.columns = ['Chennai', 'Salem']
     b1.index.name ='Weekend/Weekday preferences'
     b1.fillna(0, inplace=True)
+    b1 = b1.apply(pd.to_numeric)
     st.dataframe(b1)
 
 
