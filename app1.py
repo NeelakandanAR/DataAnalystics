faa077456be4886c5b7b86657ecbc2f5d6a21d4d
neels2708 <neelakandanbt@gmail.com>
2022-11-01 21:39:06 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -288,16 +288,16 @@ def main():
                 else:
                     st.warning("Incorrect Username/Password")
 
-elif choice == "SignUp":
-    st.subheader("Create New Account")
-    new_user = st.text_input("Username")
-    new_password = st.text_input("Password", type='password')
+    elif choice == "SignUp":
+        st.subheader("Create New Account")
+        new_user = st.text_input("Username")
+        new_password = st.text_input("Password", type='password')
 
-    if st.button("Signup"):
-        create_usertable()
-        add_userdata(new_user, make_hashes(new_password))
-        st.success("You have successfully created a valid Account")
-        st.info("Go to Login Menu to login")
+        if st.button("Signup"):
+            create_usertable()
+            add_userdata(new_user, make_hashes(new_password))
+            st.success("You have successfully created a valid Account")
+            st.info("Go to Login Menu to login")
 
 if __name__ == '__main__':
     main()
